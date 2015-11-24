a781c64cd553053dc539e08826472e40ae692090
AdrieanKhisbe <adriean.khisbe@live.fr>
2015-11-24 09:31:44 +0100

    ci: setup-ert

---
+++ test/test-helper.el
@@ -0,0 +1,38 @@
+;;; test-helper --- Test helper for slack-el
+
+;;; Commentary:
+;; test helper inspired from https://github.com/tonini/overseer.el/blob/master/test/test-helper.el
+
+;;; Code:
+
+(require 'f)
+
+(defvar cpt-path
+  (f-parent (f-this-file)))
+
+(defvar slack-el-test-path
+  (f-dirname (f-this-file)))
+
+(defvar slack-el-root-path
+  (f-parent slack-el-test-path))
+
+(defvar slack-el-sandbox-path
+  (f-expand "sandbox" slack-el-test-path))
+
+(when (f-exists? slack-el-sandbox-path)
+  (error "Something is already in %s. Check and destroy it yourself" slack-el-sandbox-path))
+
+(defmacro within-sandbox (&rest body)
+  "Evaluate BODY in an empty sandbox directory."
+  `(let ((default-directory slack-el-sandbox-path))
+     (when (f-exists? slack-el-sandbox-path)
+       (f-delete default-directory :force))
+     (f-mkdir slack-el-sandbox-path)
+     ,@body
+     (f-delete default-directory :force)))
+
+(require 'ert)
+(require 'slack-el)
+
+(provide 'test-helper)
+;;; test-helper.el ends here
